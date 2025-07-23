39fc4c181397d8107dded503ffbc141bafcde835
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-07-23 23:40:48 +0200

    modified src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './style.css'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
